0a4fbb8d56040966550ddeab59707f42dd1a0516
Your Name <you@example.com>
2022-12-12 14:54:51 -0800

    asda

--- Game.java
+++ Game.java
@@ -9,7 +9,7 @@ import java.awt.*;
 public class Game extends JPanel {
 	
    private int myWindowWidth = 700;
-   private int myWindowHeight = 450;
+   private int myWindowHeight = 550;
 	
    public Game() {
       JFrame easel = new JFrame();		
@@ -30,7 +30,51 @@ public class Game extends JPanel {
       g.drawRect(95, 60, 50, 20);
 
       // 1 2 3 4 5 6 section
-      g.drawRect(30, 155, 90, 20);
+      g.drawRect(30, 150, 90, 20);
+      g.drawRect(130, 150, 90, 20);
+
+      g.drawRect(30, 180, 90, 20);
+      g.drawRect(130, 180, 90, 20);
+
+      g.drawRect(30, 210, 90, 20);
+      g.drawRect(130, 210, 90, 20);
+
+      g.drawRect(30, 240, 90, 20);
+      g.drawRect(130, 240, 90, 20);
+
+      g.drawRect(30, 270, 90, 20);
+      g.drawRect(130, 270, 90, 20);
+
+      g.drawRect(30, 300, 90, 20);
+      g.drawRect(130, 300, 90, 20);
+
+      // SubTOTAL,BONUS-- BOXES
+      g.drawRect(130, 340, 100, 20);
+      g.drawRect(130, 370, 100, 20);
+      g.drawRect(130, 400, 100, 20);
+     
+
+      // SUBTOAL, BONUS---LETTERINGS-
+
+      g.drawString("SUBTOTAL", 50, 355);
+      g.drawString("BONUS", 50, 385);
+      g.drawString("UPPERTOTAL", 50, 415);
+
+         // DICE IMAGES (squares)
+
+         
+         g.drawRect(230, 40, 40, 40);
+         g.drawRect(280, 40, 40, 40);
+         g.drawRect(330, 40, 40, 40);
+         g.drawRect(380, 40, 40, 40);
+         g.drawRect(430, 40, 40, 40);
+
+         g.drawRect(445, 85, 10, 10);
+         g.drawRect(245, 85, 10, 10);
+         g.drawRect(295, 85, 10, 10);
+         g.drawRect(345, 85, 10, 10);
+         g.drawRect(395, 85, 10, 10);
+
 
 
       // try{
